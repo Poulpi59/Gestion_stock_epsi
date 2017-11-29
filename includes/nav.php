0184b9e4a8fe08ea051e70c40e10cf8331ccfa8e
sigdemo80 <sigfried.demory@gmail.com>
2017-11-29 15:49:45 +0100

    typo

--- includes/nav.php
+++ includes/nav.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <ul class="nav nav-justified">
             <li><a href="../index.php"><h4>Accueil</h4></a></li>
-            <li><a href="emprunt.php"><h4>Emprunt</h4></a></li>
+            <li><a href="emprunt.php"><h4>Emprunts</h4></a></li>
             <li><a href="donnee.php"><h4>Ajouter Matériel/Utilisateur</h4></a></li>
             <li><a href="logout.php"><h4>Déconnexion</h4></a></li>
         </ul>
